9093c710444020bd53b0edaa74f700d0e4f6c71d
JalalAlbarn96 <jalal.albarn@gmail.com>
2023-01-30 02:30:38 +0700

    validation

--- validation_test.go
+++ validation_test.go
@@ -123,3 +123,167 @@ func TestValidationCrossField(t *testing.T) {
 		}
 	}
 }
+
+// Validation Nested Struct
+func TestValidationNestedStruct(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+	type User struct {
+		Id     string  `validate:"required"`
+		Name   string  `validate:"required"`
+		Addres Address `validate:"required"`
+	}
+
+	request := User{
+		Id:   "",
+		Name: "",
+		Addres: Address{
+			City:    "",
+			Country: "",
+		},
+	}
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+// Validation Collection
+func TestCollection(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+	type User struct {
+		Id        string    `validate:"required"`
+		Name      string    `validate:"required"`
+		Addresses []Address `validate:"required,dive"`
+	}
+
+	request := User{
+		Id:   "01",
+		Name: "jalal",
+		Addresses: []Address{
+			{
+				City:    "",
+				Country: "",
+			},
+		},
+	}
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+}
+
+func TestBacisCollection(t *testing.T) {
+	type User struct {
+		Id      string   `validate:"required"`
+		Name    string   `validate:"required"`
+		Hobbies []string `validate:"dive,required,min=3"`
+	}
+	user := User{
+		Id:      "01",
+		Name:    "jalal",
+		Hobbies: []string{},
+	}
+	validation := validator.New()
+	err := validation.Struct(user)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+// Validation Map
+func TestMap(t *testing.T) {
+	type School struct {
+		Name string `validate:"required"`
+	}
+	type User struct {
+		Id      string            `validate:"required"`
+		Name    string            `validate:"required"`
+		Hobbies []string          `validate:"dive,required,min=3"`
+		Schools map[string]School `validate:"dive,keys,required,min=3,endkeys,dive"`
+	}
+	user := User{
+		Id:      "01",
+		Name:    "Jalal",
+		Hobbies: []string{"Gaming", "Coding"},
+		Schools: map[string]School{
+			"SD": {
+				Name: "",
+			},
+			"SMP": {
+				Name: "MTsN 1 ",
+			},
+		},
+	}
+	validate := validator.New()
+	err := validate.Struct(user)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestBasicMap(t *testing.T) {
+	type School struct {
+		Name string `validate:"required"`
+	}
+	type User struct {
+		Id      string            `validate:"required"`
+		Name    string            `validate:"required"`
+		Hobbies []string          `validate:"dive,required,min=3"`
+		Schools map[string]School `validate:"dive,keys,required,min=3,endkeys,dive"`
+		Wallet  map[string]int    `validate:"dive,keys,required,endkeys,required,gt=20000"`
+	}
+	user := User{
+		Id:      "01",
+		Name:    "Jalal",
+		Hobbies: []string{"Gaming", "Coding"},
+		Schools: map[string]School{
+			"SDN": {
+				Name: "SDN 01",
+			},
+			"SMP": {
+				Name: "MTsN 1 ",
+			},
+		},
+		Wallet: map[string]int{
+			"BNI": 10000,
+		},
+	}
+	validate := validator.New()
+	err := validate.Struct(user)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+// Alias Tag
+func TestAlias(t *testing.T) {
+	validation := validator.New()
+	validation.RegisterAlias("var", "required,max=10")
+
+	type Seller struct {
+		Id     string `validate:"var"`
+		Name   string `validate:"var"`
+		Owner  string `validate:"var"`
+		Slogan string `validate:"var"`
+	}
+	seller := Seller{
+		Id:     "",
+		Name:   "",
+		Owner:  "",
+		Slogan: "",
+	}
+	err := validation.Struct(seller)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
